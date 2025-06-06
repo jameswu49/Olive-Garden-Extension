29738522c947e57a855978e4acaca711c341d737
Jimmy Wu <jameswu49@gmail.com>
2025-06-05 18:37:23 -0700

    init project

---
+++ Olive-GardenTests/Olive_GardenTests.swift
@@ -0,0 +1,17 @@
+//
+//  Olive_GardenTests.swift
+//  Olive-GardenTests
+//
+//  Created by Jimmy Wu on 6/4/25.
+//
+
+import Testing
+@testable import Olive_Garden
+
+struct Olive_GardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
