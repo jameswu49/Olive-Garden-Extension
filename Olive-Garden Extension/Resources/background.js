044920ef605adb2f23feec0b64b91453f30fd81d
Jimmy Wu <jameswu49@gmail.com>
2025-06-12 19:01:39 -0700

    connect popup with background

--- Olive-Garden Extension/Resources/background.js
+++ Olive-Garden Extension/Resources/background.js
@@ -1,6 +1,12 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+let counter = 0;
 
-    if (request.greeting === "hello")
-        return Promise.resolve({ farewell: "goodbye" });
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.greeting === "hello") {
+    counter++;
+    return Promise.resolve({ farewell: "goodbye" });
+  }
+
+  if (request.status === "fetch number") {
+    return Promise.resolve({ number: counter });
+  }
 });
--- Olive-Garden Extension/Resources/popup.js
+++ Olive-Garden Extension/Resources/popup.js
@@ -1,1 +1,17 @@
 console.log("Hello World!", browser);
+
+const button = document.getElementById("button");
+
+button.addEventListener("click", async () => {
+  const counter = document.getElementById("counter");
+  counter.textContent = parseInt(counter.textContent) + 1;
+  const response = await browser.runtime.sendMessage({ greeting: "hello" });
+  const responseDiv = document.getElementById("response");
+  responseDiv.textContent = response.farewell;
+});
+
+const counterDiv = document.getElementById("counter");
+
+browser.runtime.sendMessage({ status: "fetch number" }).then((response) => {
+  counterDiv.textContent = response.number;
+});
